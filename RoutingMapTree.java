11dd487d2d068ebce9c003dcf30810552022ceb1
Anchit Tandon <mt6170772@iitd.ac.in>
2018-09-06 21:06:27 +0530

    Added RoutingMapTree.java

---
+++ RoutingMapTree.java
@@ -0,0 +1,16 @@
+public class RoutingMapTree{
+	Exchange root;
+	RoutingMapTree(){
+		root = new Exchange(0);
+	}
+	public Boolean containsNode(Exchange node){
+		if(root = node){
+			return true;
+		}
+		int n = root.numChildren();
+		for(int i=0;i<n;i++){
+			if()
+		}
+	}
+
+}
