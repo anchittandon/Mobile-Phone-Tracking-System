39cdcfc387a73077cbb455715baa5f7165564078
Anchit Tandon <mt6170772@iitd.ac.in>
2018-09-08 09:53:55 +0530

    Completed switchOff() and switchOn(), added a function to find an Exchange in a RoutingMapTree, added Incomplete implementation of performAction

--- RoutingMapTree.java
+++ RoutingMapTree.java
@@ -3,6 +3,7 @@ interface RoutingMapTree{
 	public void switchOn(MobilePhone mobile, Exchange node);
 	public void switchOff(MobilePhone mobile);
 	public void performAction(String actionMessage);
+	public void setRoot(int i);
 }
 
 
@@ -12,6 +13,10 @@ public class RoutingMapTree{
 	RoutingMapTree(){
 		root = new Exchange(0);
 	}
+	public void setRoot(int i){
+		root.setUniqueId(i);
+	}
+
 	public Boolean containsNode(Exchange node){
 		if(root = node){
 			return true;
@@ -25,7 +30,30 @@ public class RoutingMapTree{
 		}
 		return false;
 	}
-    public void switchOn(MobilePhone mobile, Exchange node)
+    public void switchOn(MobilePhone mobile, Exchange node){
+    	if(mobile.status() == false){
+    		mobile.switchOn();
+    		mobile.setBase(node);
+    		node.addMobileToResidentSet(mobile);
+    		while(node.parent() !=null){
+    			node = node.parent();
+    			node.addMobileToResidentSet(mobile);
+    		}
+    	}
+    }
+    public void switchOff(MobilePhone mobile){
+    	if(mobile.status() == true){
+    		mobile.switchOff();
+    	}
+    }
+    public void performAction(String actionMessage){
+    	String delims = "[ ]+";
+		String[] tokens = actionMessage.split(delims);
+		if(tokens[0].equals("addExchange") == true){
+			Exchange X = 
+		}
+
+    }
 
 
 
