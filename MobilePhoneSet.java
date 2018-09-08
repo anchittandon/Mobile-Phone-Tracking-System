ddcfb08070a579f626c639e4df341bf13914b169
Anchit Tandon <mt6170772@iitd.ac.in>
2018-09-08 16:24:31 +0530

    Added searchPhone() implementation

--- MobilePhoneSet.java
+++ MobilePhoneSet.java
@@ -2,4 +2,22 @@ public class MobilePhoneSet extends Myset{
 	public MobilePhoneSet(){
 		super();
     }
+     public MobilePhone searchPhone(int id){
+        MyLinkedList.Node itr = myset.head;
+        MobilePhone temp = null;
+        if(itr!=null){
+        	temp = (MobilePhone) itr.data;
+        }	
+        while(itr!=null && (temp.number() != id)){
+            itr = itr.next;
+            if(itr!=null){
+        		temp = (MobilePhone)itr.data;
+        	}
+        }
+        if(itr == null){
+            return null;
+        }
+        return temp;
+    }
+     
 }
